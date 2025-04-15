04a88c185f41233f0bde8c84274f9f37504a4d8a
Chloe M <chloe.molle@neuf.fr>
2025-04-15 10:40:35 +0200

    add logos

--- src/components/landing-pages/Partners.tsx
+++ src/components/landing-pages/Partners.tsx
@@ -1,5 +1,3 @@
-import Ademe from '@/components/images/partners/Ademe'
-import Marianne from '@/components/images/partners/Marianne'
 import { getServerTranslation } from '@/helpers/getServerTranslation'
 import Image from 'next/image'
 
@@ -8,22 +6,42 @@ export default async function Partners() {
 
   return (
     <div className="flex justify-center md:-mt-10">
-      <div className="relative flex items-center justify-center gap-6 py-6 md:gap-8 md:py-10">
-        <Marianne className="h-auto w-12 md:w-auto" />
-
-        <div>
-          <Ademe className="h-auto w-10 md:w-auto" />
-        </div>
-
-        <div>
-          <Image
-            src="/images/misc/logo-abc-web.webp"
-            alt={t("Logo de l'Association pour la transition Bas Carbone")}
-            width="90"
-            height="30"
-            className="h-auto w-20"
-          />
-        </div>
+      <div className="relative flex items-center justify-center gap-6 py-6 md:gap-8 md:py-10 md:flex-row flex-col-small">
+        <Image
+          src="/images/logos/ABC.png"
+          alt={t("Logo de l'Association pour la transition Bas Carbone")}
+          width="1200"
+          height="585"
+          className="h-fit w-32"
+        />
+        <Image
+          src="/images/logos/RQT.jpg"
+          alt={t("Logo du Réseau Quartier en Transition")}
+          width="200"
+          height="200"
+          className="h-fit w-24"
+        />
+        <Image
+          src="/images/logos/Paris.png"
+          alt={t("Logo de la ville de Paris")}
+          width="1548"
+          height="1420"
+          className="h-fit w-32"
+        />
+        <Image
+          src="/images/logos/NZC.jfif"
+          alt={t("Logo de Net Zero Cities")}
+          width="474"
+          height="474"
+          className="h-fit w-32"
+        />
+        <Image
+          src="/images/logos/UE.png"
+          alt={t("Logo de l'Union Européenne")}
+          width="4247"
+          height="891"
+          className="h-fit w-32"
+        />
       </div>
     </div>
   )
