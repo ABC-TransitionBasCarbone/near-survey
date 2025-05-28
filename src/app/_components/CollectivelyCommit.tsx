d70fe9b32a711459a77e0bd9d047759ed1080bd7
Chloe M <chloe.molle@neuf.fr>
2025-05-28 12:19:02 +0200

    add iamges

--- src/app/_components/CollectivelyCommit.tsx
+++ src/app/_components/CollectivelyCommit.tsx
@@ -1,5 +1,7 @@
 import MotivationSection from '@/components/landing-pages/MotivationSection'
 import Trans from '@/components/translation/Trans'
+import nearlogo from '../../../public/images/misc/near-logo.png'
+import Image from 'next/image'
 
 export default async function CollectivelyCommit() {
   return (
@@ -24,6 +26,7 @@ export default async function CollectivelyCommit() {
         }
       />
       <MotivationSection
+        image={nearlogo}
         title={
           <Trans>NEAR qu'est-ce que c'est ?</Trans>
         }
